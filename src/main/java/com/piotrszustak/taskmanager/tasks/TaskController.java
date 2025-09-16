4b3b6f0a5c30cdbbbf2f0e5b7793789fced1b60b
Piotr Szustak <122675331+piotrszustak@users.noreply.github.com>
2025-09-16 15:31:06 +0200

    feat(tasks): add GET /api/tasks endpoint

---
+++ src/main/java/com/piotrszustak/taskmanager/tasks/TaskController.java
@@ -0,0 +1,23 @@
+package com.piotrszustak.taskmanager.tasks;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getTasks();
+    }
+}
