eefd40e08bb00ddc8da8dcddbe2ae2d9dc0eaf7d
Piotr Szustak <122675331+piotrszustak@users.noreply.github.com>
2025-10-01 19:07:19 +0200

    feat(categories): add GET /api/categories endpoint

---
+++ src/main/java/com/piotrszustak/taskmanager/categories/CategoryController.java
@@ -0,0 +1,23 @@
+package com.piotrszustak.taskmanager.categories;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<CategoryDto> getAllCategories() {
+        return categoryService.getCategories();
+    }
+}
---
+++ src/main/java/com/piotrszustak/taskmanager/categories/CategoryService.java
@@ -0,0 +1,24 @@
+package com.piotrszustak.taskmanager.categories;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+    public CategoryService(CategoryRepository categoryRepository, CategoryMapper categoryMapper) {
+        this.categoryRepository = categoryRepository;
+        this.categoryMapper = categoryMapper;
+    }
+
+    public List<CategoryDto> getCategories() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(categoryMapper::toDto)
+                .toList();
+    }
+}
