1b5ff3788e31a6de25551e88574dec7bc1c17ffd
Piotr Szustak <122675331+piotrszustak@users.noreply.github.com>
2025-10-14 13:11:33 +0200

    feat(categories): load categories into frontend

--- src/main/resources/static/js/tasks.js
+++ src/main/resources/static/js/tasks.js
@@ -1,11 +1,33 @@
+loadAllCategories();
 loadAllTasks();
+
+async function loadAllCategories() {
+    try {
+        const response = await fetch("/api/categories");
+
+        if(!response.ok) {
+            throw new Error("Could not fetch categories");
+        }
+
+        const data = await response.json();
+        const ul = document.getElementById("categoryList");
+
+        data.forEach(category => {
+            const li = document.createElement("li");
+            li.textContent = category.name;
+            ul.appendChild(li);
+        });
+    } catch(error) {
+        console.error(error);
+    }
+}
 
 async function loadAllTasks() {
     try {
         const response = await fetch("/api/tasks");
 
         if(!response.ok) {
-            throw new Error("Could not fetch resource");
+            throw new Error("Could not fetch tasks");
         }
 
         const data = await response.json();
