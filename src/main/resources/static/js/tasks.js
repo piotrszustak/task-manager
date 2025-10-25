ef9a20d05066c66c68bc60732fee6c33c54614ac
Piotr Szustak <122675331+piotrszustak@users.noreply.github.com>
2025-10-26 00:09:57 +0200

    refactor(tasks): rename selectors in HTML and separate fetch logic for tasks and categories

--- src/main/resources/static/js/tasks.js
+++ src/main/resources/static/js/tasks.js
@@ -1,44 +1,51 @@
-loadAllCategories();
-loadAllTasks();
+// === Function definitions ===
+
+async function fetchCategories() {
+    const response = await fetch('/api/categories');
+    if (!response.ok) throw new Error('Could not fetch categories.');
+    return await response.json();
+}
 
 async function loadAllCategories() {
     try {
-        const response = await fetch("/api/categories");
-
-        if(!response.ok) {
-            throw new Error("Could not fetch categories");
-        }
-
-        const data = await response.json();
-        const ul = document.getElementById("categoryList");
+        const data = await fetchCategories();
+        const ul = document.getElementById('categories');
+        ul.innerHTML = '<li>All</li>';
 
         data.forEach(category => {
-            const li = document.createElement("li");
+            const li = document.createElement('li');
             li.textContent = category.name;
             ul.appendChild(li);
         });
-    } catch(error) {
+    } catch (error) {
         console.error(error);
     }
+}
+
+async function fetchTasks() {
+    const response = await fetch('/api/tasks');
+    if (!response.ok) throw new Error('Could not fetch tasks.');
+    return await response.json();
 }
 
 async function loadAllTasks() {
     try {
-        const response = await fetch("/api/tasks");
-
-        if(!response.ok) {
-            throw new Error("Could not fetch tasks");
-        }
-
-        const data = await response.json();
-        const ul = document.getElementById("taskList");
+        const data = await fetchTasks();
+        const ul = document.getElementById('tasks');
+        ul.innerHTML = '';
 
         data.forEach(task => {
-            const li = document.createElement("li");
-            li.textContent = task.title + ": " + task.description;
+            const li = document.createElement('li');
+            const description = task.description === null ? '' : ': ' + task.description;
+            li.textContent = task.title + description;
             ul.appendChild(li);
         });
-    } catch(error) {
+    } catch (error) {
         console.error(error);
     }
 }
+
+// === Initializations ===
+
+loadAllCategories();
+loadAllTasks();
