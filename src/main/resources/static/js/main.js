e4d175f7843b3ad73c818e5bba2e071ab8fa69ba
Piotr Szustak <122675331+piotrszustak@users.noreply.github.com>
2025-11-06 11:00:28 +0100

    refactor(js): split tasks.js into main.js, utils.js, tasks.js, categories.js

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,9 @@
+// === Imports ===
+
+import { loadAllCategories } from './categories.js';
+import { loadAllTasks } from './tasks.js';
+
+// === Initializations ===
+
+loadAllCategories();
+loadAllTasks();
